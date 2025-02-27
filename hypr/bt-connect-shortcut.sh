a00e2fb5211d265f7c67b83237aa1c7c85b1077f
aem2231 <mariojetullaji@gmail.com>
2025-02-27 15:31:12 +0000

    idk what to say

--- hypr/bt-connect-shortcut.sh
+++ hypr/bt-connect-shortcut.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-bluetoothctl connect D0:56:FB:50:50:53 &
 bluetoothctl connect 3C:FA:06:15:C9:02
--- hypr/start.sh
+++ hypr/start.sh
@@ -3,15 +3,12 @@
 #initialize wallpaper daemon
 swww init &
 #set wallpaper
-swww img ~/Wallpapers/mandelbrot.png &
+swww img ~/Wallpapers/space.png &
 
 #network manager
 nm-applet --ibdicator &
 
 #start waybar and notifdaemon
 waybar &
-dunst &
-
-#start other servicees
-bluetoothctl connect DC:69:E2:A8:44:21
+dunst
 
